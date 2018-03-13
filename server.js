f84d4535d428ced831d1f1864da1699091fc8cf6
Depew <depewj@SVCCISDEPT>
2018-03-13 13:25:11 -0400

    Commit files

---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+// blog page 
+app.get('/blog', function(req, res) {
+    res.render('pages/blog');
+});
+
+// articles page 
+app.get('/articles', function(req, res) {
+    res.render('pages/articles');
+});
+
+// contact us page 
+app.get('/contact', function(req, res) {
+    res.render('pages/contact');
+});
+
+// Enter localhost:3001 into your browser
+app.listen(3001);
+console.log('Listening on port 3001');
